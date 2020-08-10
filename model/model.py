b54048d86f7f6ce9d8618621a0c5737cd4bdecce
miyazaki.carp <miyazaki.carp@gmail.com>
2020-08-10 22:04:28 +0900

    mean/std

--- model/model.py
+++ model/model.py
@@ -5,63 +5,27 @@ import torch.nn.functional as F
 
 
 class DogClassificationModel(nn.Module):
-    def __init__(self, model, num_classes, mean=0.5, std=0.25):
+    def __init__(self, model, num_classes, batch_norm=False):
         super(DogClassificationModel, self).__init__()
         self.num_classes = num_classes
-        self.mean = mean
-        self.std = std
+        self.batch_norm = batch_norm
 
         # base model (e.g. ResNet50, ResNet152, ResNeXt, ... and so on.)
         self.base_model = model
         num_ftrs = model.fc.in_features
         self.base_model.fc = nn.Linear(num_ftrs, self.num_classes)
 
+
     def forward(self, x):
-        x = (x - self.mean) / self.std 
+        mean = 0.5
+        std = 0.25
+        if self.batch_norm:
+            mean = torch.mean(x)
+            std = torch.std(x)
+
+        x = (x - mean) / std 
         x = self.base_model(x)
         return x
-
-
-class OrgModel(nn.Module):
-    def __init__(self, num_classes, attention=False):
-        super(OrgModel, self).__init__()
-        self.attention = attention
-
-        self.base_model = models.resnet50(pretrained=True)
-        layers = list(self.base_model.children())[:4]
-        self.model0 = nn.Sequential(*layers)
-        self.model1 = list(self.base_model.children())[4]
-
-        self.fc1 = nn.Linear(256, 64)
-        self.relu = nn.ReLU()
-        self.fc2 = nn.Linear(64, 256)
-        self.sigmoid = nn.Sigmoid()
-
-        layers2 = list(self.base_model.children())[5:9] 
-        self.model2 = nn.Sequential(*layers2)
-
-        self.fc = nn.Linear(2048, num_classes)
-
-
-    def forward(self, x):
-        x = (x - 0.5) / 0.25
-        x = self.model0(x)
-        x = self.model1(x)
-        y = F.adaptive_max_pool2d(x, (1, 1)).squeeze(2).squeeze(2)
-        y = self.fc1(y)
-        y = self.relu(y)
-        y = self.fc2(y)
-        y = self.sigmoid(y)
-        y = y.view(-1, 256, 1, 1)
-
-        z = x
-        if self.attention:
-            z = x * y
-        x = self.model2(z)
-        x = x.view(-1, 2048)
-        x = self.fc(x)
-
-        return x, z 
 
 
 class ExtendedModelForCoreML(nn.Module):
